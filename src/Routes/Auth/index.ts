a1538ba6ded66b8d012793791bc9ce76a45a00b1
Adarsh A J <68109191+Adarshspidey@users.noreply.github.com>
2022-06-17 10:43:32 +0530

    login validation

--- src/Routes/Auth/index.ts
+++ src/Routes/Auth/index.ts
@@ -7,15 +7,20 @@ import OtpValidation from "./Validation/otp";
 import Login, { IsUserExist } from "./Controllers/Login";
 import Otp, { IsOtpVerified } from "./Controllers/Otp";
 
-router.post("/sign-up/validate", SignUpValidation,(req, res) => {
+router.post("/sign-up/validate", SignUpValidation, (req, res) => {
   res.status(200).json({
     message: "SignUp Validation Successfully",
     data: {},
   });
-  }
-);
+});
 router.post("/sign-up", SignUpValidation, SignUp);
 
+router.post("/login/validate", LoginValidation, (req, res) => {
+  res.status(200).json({
+    message: "Login Validation Successfully",
+    data: {},
+  });
+});
 router.post("/login", LoginValidation, IsUserExist, Login);
 
 router.get("/forgot-password", (req, res) => {
